4fac9ef1a667fd42e09b42ff29b8c01064848f23
DylanPiriou <dylan.piriou@hotmail.com>
2023-06-27 23:28:48 +0200

    count vowels

--- hello.py
+++ hello.py
@@ -10,8 +10,17 @@
 # print("La moyenne est :", moyenne)
 
 # Vérification de palindrome
-mot = input("Veuillez saisir un mot : ")
-if (mot == mot[::-1]):
-    print ("C'est une phrase palindromique")
-else:
-    print ("Ce n'est pas une phrase palindromique.")
+# mot = input("Veuillez saisir un mot : ")
+# if (mot == mot[::-1]):
+#     print ("C'est une phrase palindromique")
+# else:
+#     print ("Ce n'est pas une phrase palindromique.")
+
+# Compter le nombre de voyelles
+voyelles = ["a", "e", "i", "o", "u"]
+phrase = input("Veuillez entrer une phrase : ")
+nb_voyelles = 0
+for lettre in phrase.lower():
+    if lettre in voyelles:
+        nb_voyelles += 1
+print(f"Il y a {nb_voyelles} voyelles dans la phrase.")
