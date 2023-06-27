494200ff2adbac480c80f8f278e0a9f5dcfbe96a
DylanPiriou <dylan.piriou@hotmail.com>
2023-06-27 23:19:48 +0200

    logic palindrome

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 # Calcul de moyenne
-n = int(input("Combien de nombres voulez-vous entrer ? "))
-total = 0
+# n = int(input("Combien de nombres voulez-vous entrer ? "))
+# total = 0
 
-for i in range(n):
-    nombre = float(input(f"Entrez le nombre {i+1} : "))
-    total += nombre
+# for i in range(n):
+#     nombre = float(input(f"Entrez le nombre {i+1} : "))
+#     total += nombre
 
-moyenne = total / n
-print("La moyenne est :", moyenne)
+# moyenne = total / n
+# print("La moyenne est :", moyenne)
+
+# Vérification de palindrome
+mot = input("Veuillez saisir un mot : ")
+if (mot == mot[::-1]):
+    print ("C'est une phrase palindromique")
+else:
+    print ("Ce n'est pas une phrase palindromique.")
